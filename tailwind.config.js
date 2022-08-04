eed093be77802be9f58c718ab513ba0714341317
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2022-08-04 19:44:17 +0800

    tailwind exercise

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'outift_300':[ 'Outfit', 'sans-serif'],
+      'outfit_700':[ 'Outfit', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
